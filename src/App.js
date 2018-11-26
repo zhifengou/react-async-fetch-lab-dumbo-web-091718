a29378ca43a9dc1617ac86c5a1ea3942404f8aa4
zhifeng ou <zhifengou@hotmail.com>
2018-11-26 01:24:38 +0000

    automatically backed up by learn

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
 // create your App component here
+import React,{Component} from 'react'
+
+export default class App extends Componet {
+  state ={
+    people: []
+  }
+  render(){
+    return(
+      <div>
+      this.state.people.map((ppl,index)=>(<div key=index>{ppl.name}</div>))
+      </div>
+      )
+  }
+}
